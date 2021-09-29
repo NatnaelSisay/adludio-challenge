f0c77a894dc9cf916b7a2cca4cf5078875a8739d
NatnaelSisay <natyjava8@gmail.com>
2021-09-29 20:22:14 +0300

    basic front-end and backend connection

--- web/adludio-front/src/App.js
+++ web/adludio-front/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import axios from 'axios';
+
+// Fetch data from server and setup on the drow down
+// send a request and check the result
 
 function App() {
+  const [codes, setSoces] = useState()
+  useEffect(() => {
+      axios.get('http://127.0.0.1:5000/').then(res => {
+        
+        // console.log(res.data['CampainId'])
+        let result = res.data['CampainId']
+        setSoces(result)
+      })
+  },[])
+
+  console.log(codes)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+      {  codes?.map((element, index) => {
+        return <li key={index}>{element}</li>
+      }) }
+      </ul>
+      
     </div>
   );
 }
