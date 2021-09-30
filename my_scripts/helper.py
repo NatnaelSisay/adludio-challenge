c6aa9fd9897a59de48f33eaa0e8906dd30c03f8c
NatnaelSisay <natyjava8@gmail.com>
2021-09-30 18:16:39 +0300

    cleaning up codes

--- my_scripts/helper.py
+++ my_scripts/helper.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
 
 
 def best_performers(df, campain_id):
@@ -48,3 +51,49 @@ def bar_plot(x, y, df):
   plt.figure(figsize=(12,6))
   plt.xticks(rotation=20)
   sns.barplot(x=x, y=y, data=df)
+
+
+def oneHotEncoder(df, catagorical_columns):
+    new_df = df.copy()
+    new_df = pd.get_dummies(new_df, columns=catagorical_columns)
+    return new_df
+
+
+def labelEncoder(df,catagorical_columns):
+    new_df = df.copy()
+    for column in catagorical_columns:
+            new_df[column] = LabelEncoder().fit_transform(new_df[column])
+    return new_df
+
+
+def train_model(train, test, model_type='Linear'):
+    model = LinearRegression()
+    
+    if (model_type == 'Logistic'):
+        model = LogisticRegression()
+        
+    model.fit(train, test)
+    return model
+
+
+def test_model(x_test, y_test, model):
+    score = model.score(x_test, y_test)
+    print("Model Score => ", score)
+
+
+def logistic_featuer_importance(model, columns):
+    importance = model.coef_[0]
+    df = pd.DataFrame([list(importance)], columns=columns)
+    df = df.transpose()
+    df = df.reset_index()
+    df.rename(columns={'index':'columns', 0:'score'}, inplace=True)
+    return df
+
+
+def linear_feature_importance(model, columns):
+    importance = model.coef_
+    df = pd.DataFrame([list(importance)], columns=columns)
+    df = df.transpose()
+    df = df.reset_index()
+    df.rename(columns={'index':'columns', 0:'score'}, inplace=True)
+    return df
