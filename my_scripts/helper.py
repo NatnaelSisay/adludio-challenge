e23a0714f743330857b57a15356bc3a5b8c2fb6e
NatnaelSisay <natyjava8@gmail.com>
2021-10-01 06:52:53 +0300

    minor algorithm modification

--- my_scripts/helper.py
+++ my_scripts/helper.py
@@ -21,13 +21,13 @@ def best_performers(df, campain_id):
     selcted_campain = campains_score.groupby('Site').agg({'engagement': 'sum', 'LogEntryTime':'count'})
     selcted_campain.rename(columns={'LogEntryTime': 'engagement-count'}, inplace=True)
     
-    # method - 1
-    # selcted_campain['score'] = selcted_campain['engagement'] / selcted_campain['engagement-count']
+    #method - 1
+    selcted_campain['score'] = selcted_campain['engagement'] / selcted_campain['engagement-count']
     
     #method - 2
-    selcted_campain['score'] = (selcted_campain['engagement'] ** 2) / selcted_campain['engagement-count']
+    # selcted_campain['score'] = (selcted_campain['engagement'] ** 2) / selcted_campain['engagement-count']
     
-    result = selcted_campain.sort_values(by='engagement', ascending=False).reset_index()
+    result = selcted_campain.sort_values(by=['engagement', 'score'], ascending=False).reset_index()
     
     return result
 
